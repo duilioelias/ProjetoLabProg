4edbc822a523b1398d847b35032c3c8151c29bff
Duilio Elias <duilioelias@gmail.com>
2015-05-18 22:07:11 -0300

    tirei o filename dos parametros de funções no qual ele nao estava sendo usado

--- funcoes.h
+++ funcoes.h
@@ -56,7 +56,7 @@ void afunda_destroyer (char filename[], char** M, int m, int n, int x, int y, ch
 void afunda_cruzador (char filename[], char** M, int m, int n, int x, int y, char alvo);
 void afunda_porta_aviao (char filename[], char** M, int m, int n, int x, int y, char alvo);
 void afunda_hidro_aviao (char** M, int m, int n, int x, int y, char alvo);
-void afunda_embarcacao (char filename[], char** M, int m, int n, int x, int y, char alvo);
+void afunda_embarcacao (char** M, int m, int n, int x, int y, char alvo);
 int dispara_tiros(char filename[],char** M, int m, int n);
 
 void posiciona_barco (char filename[], char** M, int* xBarco, int* yBarco, int m, int n);
